a65e374a5b721c80629a055cbecb09d373e0d0d3
Kapaika <bartosz.kupajski@gmail.com>
2019-07-09 13:49:16 +0200

    Add path to the Main class and Book entity model

--- database/src/main/java/edition/academy/seventh/models/Book.java
+++ database/src/main/java/edition/academy/seventh/models/Book.java
@@ -1,8 +1,72 @@
 package edition.academy.seventh.models;
 
-/**
- * @author Kamil Rojek
- */
-class Book {
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Kamil Rojek, Bartosz Kupajski
+ */
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+
+    String title;
+
+    String subtitle;
+
+    String authors;
+
+    String price;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", subtitle='" + subtitle + '\'' +
+                ", authors='" + authors + '\'' +
+                ", price='" + price + '\'' +
+                '}';
+    }
 }
