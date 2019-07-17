edec78f20a771a61ec79fb7dfb02ab8d2f6dbad8
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-07-17 12:26:24 +0200

    Sprint#2, Task#66, refactor BookRepository

--- src/main/java/edition/academy/seventh/repository/BookRepository.java
+++ src/main/java/edition/academy/seventh/repository/BookRepository.java
@@ -23,6 +23,7 @@ public class BookRepository {
   private EntityManager entityManager;
   private ConnectorProvider connectorProvider;
 
+
   public BookRepository() {
     connectorProvider = ConnectorFactory.of(POSTGRESQL);
   }
