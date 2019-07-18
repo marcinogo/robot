c4b1e1a05ed7acbbe30e1880987e412dadd282c8
twojatata <patrykkuchar@gmail.com>
2019-07-18 11:56:20 +0200

    Add logger to EmpikScrapper class

--- src/main/java/edition/academy/seventh/service/EmpikScrapper.java
+++ src/main/java/edition/academy/seventh/service/EmpikScrapper.java
@@ -1,18 +1,16 @@
 package edition.academy.seventh.service;
 
 import edition.academy.seventh.database.model.Book;
-import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Phaser;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.*;
 
 /**
  * Scraps data from empik.com bookstore website in sales section using JSoup library.
@@ -22,18 +20,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmpikScrapper implements PromotionProvider {
 
+  private static final Logger logger = LoggerFactory.getLogger(EmpikScrapper.class);
   private List<Book> listOfBooks = new CopyOnWriteArrayList<>();
   private ExecutorService service = Executors.newFixedThreadPool(40);
   private Phaser phaser = new Phaser(1);
   private int numberOfPhase = 0;
 
   /**
-   * Index in for loop is incremented by 30 because of the fact that every page URL of empik sales
-   * section contains id which is incremented by 30.
+   * Index in "for" loop is incremented by 30 since every URL page of Empik's sales
+   * section contains id which is multiplicity of 30.
    *
    * <p>Loop condition is 30 books per page x 20 pages, can be changed later.
    *
-   * @return list of books after all threads finish their jobs
+   * @return list of books after all scrapping threads finish their jobs.
    */
   @Override
   public List<Book> getPromotions() {
@@ -45,25 +44,24 @@ public class EmpikScrapper implements PromotionProvider {
     try {
       phaser.awaitAdvanceInterruptibly(numberOfPhase, 5000, TimeUnit.MILLISECONDS);
     } catch (TimeoutException | InterruptedException e) {
-      System.err.println(
-          "Could not scrap every page from empik. Anyway - returned what was already scrapped successfully");
+      logger.error("Could not scrap every page from empik. Anyway - returned what was already scrapped successfully");
     }
     numberOfPhase++;
 
     return listOfBooks;
   }
 
-  private Runnable createScrappingTask(int searchSiteNumber) {
+  private Runnable createScrappingTask(int numberOfSiteToBeSearched) {
     return () -> {
       phaser.register();
-      String url = createUrl(searchSiteNumber);
+      String url = createUrl(numberOfSiteToBeSearched);
       Document document = null;
       try {
         document = Jsoup.connect(url).timeout(0).get();
       } catch (IOException exception) {
-        System.err.println(exception.getMessage());
+        logger.error(exception.getMessage());
       }
-      Elements elementsByClass = document.getElementsByClass("productWrapper");
+      Elements elementsByClass = Objects.requireNonNull(document).getElementsByClass("productWrapper");
 
       mappingToBookList(elementsByClass);
     };
