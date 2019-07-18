8695d180a3a8c66b58dbc22083e89352888cbc75
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-07-18 09:47:17 +0200

    Sprint#2, Task#71 refactor
    - add EmpikScrapper

--- src/main/java/edition/academy/seventh/controller/MainController.java
+++ src/main/java/edition/academy/seventh/controller/MainController.java
@@ -1,9 +1,7 @@
 package edition.academy.seventh.controller;
 
 import edition.academy.seventh.database.model.Book;
-import edition.academy.seventh.serivce.BookService;
-import edition.academy.seventh.serivce.BookstoreConnectionService;
-import edition.academy.seventh.serivce.ItBookMapper;
+import edition.academy.seventh.serivce.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.List;
 
-/** @author krzysztof.kramarz */
+/**
+ * Controller for starting persists actions Starting is possible by HTTP request or schedule action
+ *
+ * @author krzysztof.kramarz
+ */
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 class MainController {
@@ -22,7 +24,7 @@ class MainController {
   private BookService bookService;
 
   @Autowired
-  public MainController(
+  MainController(
       BookstoreConnectionService bookstoreConnectionService,
       ItBookMapper itBookMapper,
       BookService bookService) {
@@ -31,13 +33,28 @@ class MainController {
     this.bookService = bookService;
   }
 
+  /**
+   * Controller for starting books persistence
+   *
+   * @return result of persist action
+   */
   @GetMapping("/start")
   public boolean startRobot() {
     return scheduleRobot();
   }
 
-  @SuppressWarnings({"WeakerAccess", "BooleanMethodNameMustStartWithQuestion"})
-  public boolean scheduleRobot() {
+  /**
+   * Schedules books persistence
+   *
+   * @return result of persist action
+   */
+  boolean scheduleRobot() {
+    boolean persistItBookStore = persistItBookStore();
+    boolean persistEmpik = persistEmpik();
+    return persistItBookStore && persistEmpik;
+  }
+
+  private boolean persistItBookStore() {
     List<String> listOfBooksAsString = bookstoreConnectionService.getListOfBooksAsString();
     List<Book> books;
 
@@ -48,7 +65,16 @@ class MainController {
       return false;
     }
 
-    bookService.addBooksToDataBase(books);
+    bookService.addBooksToDatabase(books);
+    return true;
+  }
+
+  private boolean persistEmpik() {
+    // TODO zmienić na autowired, gdy EmpikScrapper będzie beanem
+    PromotionProvider promotionProvider = new EmpikScrapper();
+    List<Book> books = promotionProvider.getPromotions();
+    bookService.addBooksToDatabase(books);
+    // TODO wrpowadzić try catch i zwracać true/false po zrobieni zadania #118
     return true;
   }
 }
