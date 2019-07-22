454649f5c51d1b3470372989d0c428f9d88515d7
Ozonek95 <kacperstaszek1995@gmail.com>
2019-07-22 11:42:48 +0200

    Sprint#3, Task#132, Stworzenie dokumentacji do zadania.

--- src/main/java/edition/academy/seventh/service/scrapper/AbstractScrapper.java
+++ src/main/java/edition/academy/seventh/service/scrapper/AbstractScrapper.java
@@ -16,6 +16,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Implements sharded behaviour of all Scrapping classes.
+ *
  * @author Kacper Staszek
  */
 abstract class AbstractScrapper implements PromotionProvider {
@@ -35,6 +37,16 @@ abstract class AbstractScrapper implements PromotionProvider {
     this.documentClassName = documentClassName;
   }
 
+  /**
+   * Create {@link Runnable} task witch is responsible to create GET request
+   * to given page based on {@link AbstractScrapper#startOfUrl}
+   * and {@link AbstractScrapper#endOfUrl}, then extract all {@link org.jsoup.nodes.Element}
+   * of the given name {@link AbstractScrapper#documentClassName}
+   *
+   * @param numberOfSearchedSite number of site witch is concat to the URL.
+   *
+   * @return task witch is later use in {@link ExecutorService}.
+   */
   Runnable createScrappingTask(int numberOfSearchedSite) {
 
     return () -> {
@@ -57,5 +69,10 @@ abstract class AbstractScrapper implements PromotionProvider {
     return startOfUrl + numberOfSearchedSite + endOfUrl;
   }
 
+  /**
+   * Responsible for mapping HTML content to {@link List<Book>}
+   *
+   * @param elementsByClass main HTML element thant contains all required data.
+   */
   abstract void mappingToBookList(Elements elementsByClass);
 }
