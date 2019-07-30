a0fde3ccc7a578f7d836e4a40414cfd1a6d1cea0
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-07-30 13:03:29 +0200

    Sprint#3, Task#149 bugfix
    Test EagerBookPaginationTest

--- src/test/java/edition/academy/seventh/display/EagerBookPaginationTest.java
+++ src/test/java/edition/academy/seventh/display/EagerBookPaginationTest.java
@@ -23,47 +23,130 @@ public class EagerBookPaginationTest {
 
   @Test
   public void should_return20BooksFromCurrentPage_when_25BooksWereLoadedFromDatabase() {
-    //Given
+    // Given
     EagerBookPagination eagerBookPagination;
     List<BookDto> books = generateListOfDtoBooks(25);
 
-    //When
+    // When
     when(bookService.getBooksFromDatabase()).thenReturn(books);
     eagerBookPagination = new EagerBookPagination(bookService);
     List<BookDto> pageRecords = eagerBookPagination.currentPage();
 
-    //Then
+    // Then
     assertEquals(pageRecords.size(), 20);
   }
 
   @Test
+  public void should_return15BooksFromCurrentPage_when_15BooksWereLoadedFromDatabase() {
+    // Given
+    EagerBookPagination eagerBookPagination;
+    List<BookDto> books = generateListOfDtoBooks(15);
+
+    // When
+    when(bookService.getBooksFromDatabase()).thenReturn(books);
+    eagerBookPagination = new EagerBookPagination(bookService);
+    List<BookDto> pageRecords = eagerBookPagination.currentPage();
+
+    // Then
+    assertEquals(pageRecords.size(), 15);
+  }
+
+  @Test
   public void should_return0BooksFromCurrentPage_when_NoBooksWereLoadedFromDatabase() {
-    //Given
+    // Given
     EagerBookPagination eagerBookPagination;
     List<BookDto> emptyList = new ArrayList<>();
 
-    //When
+    // When
     when(bookService.getBooksFromDatabase()).thenReturn(emptyList);
     eagerBookPagination = new EagerBookPagination(bookService);
     List<BookDto> pageRecords = eagerBookPagination.currentPage();
 
-    //Then
+    // Then
     assertEquals(pageRecords.size(), 0);
   }
 
-  @Test(enabled = false) //todo test nie przechodzi, naprawić kod
+  @Test
   public void should_return10BooksFromNextPage_when_30BooksWereLoadedFromDatabase() {
-    //Given
+    // Given
     EagerBookPagination eagerBookPagination;
     List<BookDto> books = generateListOfDtoBooks(30);
 
-    //When
+    // When
     when(bookService.getBooksFromDatabase()).thenReturn(books);
     eagerBookPagination = new EagerBookPagination(bookService);
     List<BookDto> pageRecords = eagerBookPagination.nextPage();
 
-    //Then
+    // Then
     assertEquals(pageRecords.size(), 10);
+  }
+
+  @Test
+  public void should_return17BooksFrom2NextPage_when_57BooksWereLoadedFromDatabase() {
+    // Given
+    EagerBookPagination eagerBookPagination;
+    List<BookDto> books = generateListOfDtoBooks(57);
+
+    // When
+    when(bookService.getBooksFromDatabase()).thenReturn(books);
+    eagerBookPagination = new EagerBookPagination(bookService);
+    List<BookDto> pageRecords = eagerBookPagination.nextPage();
+    List<BookDto> pageRecordsNext = eagerBookPagination.nextPage();
+
+    // Then
+    assertEquals(pageRecords.size(), 20);
+    assertEquals(pageRecordsNext.size(), 17);
+  }
+
+
+  @Test
+  public void should_return20BooksFromNextPage_when_20BooksWereLoadedFromDatabase() {
+    // Given
+    EagerBookPagination eagerBookPagination;
+    List<BookDto> books = generateListOfDtoBooks(20);
+
+    // When
+    when(bookService.getBooksFromDatabase()).thenReturn(books);
+    eagerBookPagination = new EagerBookPagination(bookService);
+    List<BookDto> pageRecords = eagerBookPagination.nextPage();
+
+    // Then
+    assertEquals(pageRecords.size(), 20);
+  }
+
+  @Test
+  public void should_return20BooksFromNextPreviousPage_when_20BooksWereLoadedFromDatabase() {
+    // Given
+    EagerBookPagination eagerBookPagination;
+    List<BookDto> books = generateListOfDtoBooks(20);
+
+    // When
+
+    when(bookService.getBooksFromDatabase()).thenReturn(books);
+    eagerBookPagination = new EagerBookPagination(bookService);
+    eagerBookPagination.nextPage();
+    List<BookDto> pageRecords = eagerBookPagination.previousPage();
+
+    // Then
+    assertEquals(pageRecords.size(), 20);
+  }
+
+  @Test
+  public void should_return20BooksFromNextPreviousPage_when_30BooksWereLoadedFromDatabase() {
+    // Given
+    EagerBookPagination eagerBookPagination;
+    List<BookDto> books = generateListOfDtoBooks(30);
+
+    // When
+
+    when(bookService.getBooksFromDatabase()).thenReturn(books);
+    eagerBookPagination = new EagerBookPagination(bookService);
+    List<BookDto> pageRecordsNext = eagerBookPagination.nextPage();
+    List<BookDto> pageRecordsPreviuos = eagerBookPagination.previousPage();
+
+    // Then
+    assertEquals(pageRecordsNext.size(), 10);
+    assertEquals(pageRecordsPreviuos.size(), 20);
   }
 
   @Test
