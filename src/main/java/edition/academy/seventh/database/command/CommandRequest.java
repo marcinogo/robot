524e99d3e5d72aa01f83db71b2d698afee0df9ef
Krzysztof Kramarz <krzysztof.kramarz@silvermedia.pl>
2019-08-02 11:06:43 +0200

    Sprint#3, Task#241 refactor

--- src/main/java/edition/academy/seventh/database/command/CommandRequest.java
+++ src/main/java/edition/academy/seventh/database/command/CommandRequest.java
@@ -3,6 +3,6 @@ package edition.academy.seventh.database.command;
 /** @author krzysztof.kramarz */
 // TODO czy ma sens?
 public interface CommandRequest<T> {
-
+//metoda może sie nazywać run
   void accept(T command);
 }
