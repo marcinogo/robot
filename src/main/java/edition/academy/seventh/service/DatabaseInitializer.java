f5c3a395835e1526816dd44486b53f3b15fc9586
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-08-12 10:49:26 +0200

    Sprint#5, Task#216, usunięcie danych wrażliwych

--- src/main/java/edition/academy/seventh/service/DatabaseInitializer.java
+++ src/main/java/edition/academy/seventh/service/DatabaseInitializer.java
@@ -31,39 +31,4 @@ public class DatabaseInitializer {
     connectorProvider = ConnectorFactory.of(POSTGRESQL);
     this.authenticationService = authenticationService;
   }
-  /* TODO: 30.07.1 Nie usuwać - do poprawy w następnym sprincie. Przy każdym uruchomieniu aplikacji, program próbuje dodać role
-  // TODO: i konto administratora. Po pierwszym uruchomieniu one już tam są, przez co wyrzuca nam wyjątek.
-  // TODO: chwilowo rozwiązane tak, że dodaliśmy ich raz i zakomentowaliśmy kod, który próbuje to robić przy każdym następnym uruchomienu.
-   */
-  //
-
-  //  /**
-  //   * Populates database with {@link edition.academy.seventh.security.model.RoleName roles}
-  //   * and adds hardcoded ROLE_ADMIN users.
-  //   */
-  //  @PostConstruct
-  //  public void populateDatabase() {
-  //    addUserRolesToDatabase();
-  //    addAdminsToDatabase();
-  //  }
-
-  private void addAdminsToDatabase() {
-    LOGGER.info("Adding admins");
-    authenticationService.createNewAccount(
-        new RegisterForm(
-            "pan@pawel.com", "pan_pawel",
-                  Set.of("admin"), "tajnehaslo"));
-  }
-
-  private void addUserRolesToDatabase() {
-    entityManager = connectorProvider.getEntityManager();
-    LOGGER.info("Adding user roles");
-    entityManager.getTransaction().begin();
-    Query nativeQuery = entityManager.createNativeQuery(
-            "INSERT INTO role(name) VALUES('ROLE_ADMIN');\n"
-                    + "INSERT INTO role(name) VALUES('ROLE_USER');");
-    nativeQuery.executeUpdate();
-    entityManager.getTransaction().commit();
-    entityManager.close();
-  }
 }
