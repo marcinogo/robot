d2c7fb3d9454732e7cb49afd656ccc66c4fd3987
Marcin Ogorzalek <Marcin_Ogorzalek@epam.com>
2019-08-23 09:01:14 +0200

    Poprawienie javadoc

--- src/main/java/edition/academy/seventh/security/response/JwtResponse.java
+++ src/main/java/edition/academy/seventh/security/response/JwtResponse.java
@@ -24,8 +24,7 @@ public class JwtResponse {
    * Creates JwtResponse with required properties and toked which will be held locally by the client
    * user. Contains {@link GrantedAuthority authorities} defining user privileges.
    *
-   * @param accessToken Generated JSON web token {@link // *
-   *     JwtProvider#generateJwtToken(Authentication)}.
+   * @param accessToken Generated JSON web token JwtProvider.generateJwtToken(Authentication).
    * @param username of a user attempting to log in.
    * @param authorities user privileges.
    */
