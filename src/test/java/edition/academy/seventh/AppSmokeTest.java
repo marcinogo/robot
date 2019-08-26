9588c93098bc1ea56351a0ca804b9703182b5ad5
Kapaika <bartosz.kupajski@gmail.com>
2019-08-26 15:14:24 +0200

    Sprint#6, Task#283, wprawdza zmainy po recenzji; testpropertysource  i usuniećie junit z poma

--- src/test/java/edition/academy/seventh/AppSmokeTest.java
+++ src/test/java/edition/academy/seventh/AppSmokeTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @TestPropertySource(locations = "classpath:test.properties")
 @SpringBootTest
-public class SmokeTest {
+public class AppSmokeTest {
 
   @Test
   public void should_loadContext_when_executeThisMethod() throws Exception {}
--- src/test/java/edition/academy/seventh/persistence/BookRetrieverSmokeTest.java
+++ src/test/java/edition/academy/seventh/persistence/BookRetrieverSmokeTest.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource(locations = "classpath:test.properties")
 @SpringBootTest
 public class BookRetrieverSmokeTest {
 
